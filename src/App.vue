52f37312d2a548d7898a08d602ba8f65d3992426
Davud <davud@MacBook-Pro-Davud.local>
2024-10-01 11:36:32 +0500

    component_created:header_menu

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Header_Menu from "./components/UI/header_menu/Header_Menu.vue";
+import "./App.css";
+
+let id = 0;
+const menuText = [
+  { id: id++, text: "Home" },
+  { id: id++, text: "Episodes" },
+  { id: id++, text: "About" },
+  { id: id++, text: "Contact" },
+];
+</script>
+
+<template>
+  <div class="header">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+
+    <Header_Menu class="header_menu" :nameMenu="menuText" />
+  </div>
+</template>
---
+++ src/components/ui/header_menu/header_menu.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import "./Header_Menu.css";
+
+defineProps<{
+  nameMenu: { id: number; text: string, [key: string]: any}[];
+}>();
+
+</script>
+
+<template>
+  <nav>
+    <ul class="text">
+      <li v-for="menu in nameMenu" :key="menu.id">
+        <button>{{ menu.text }}</button>
+      </li>
+    </ul>
+  </nav>
+
+</template>
